e918724da84c74ab52ece2535fc3be5b0e25c242
vlasova106 <62952426+vlasova106@users.noreply.github.com>
2021-05-07 16:22:51 +0600

    commit

--- add.php
+++ add.php
@@ -1,18 +1,16 @@
 <html>
 
 <form  method="POST">
-    <input type="text" name="un">
-    <input type="text" name="faculty">
-    <input type="text" name="course">
-    <input type="text" name="subj">
-    <input type="text" name="theme">
-    <input type="text" name="content">
+    <input type="text" name="un" placeholder="Учебное заведение">
+    <input type="text" name="faculty" placeholder="Специальность">
+    <input type="text" name="course" placeholder="Курс">
+    <input type="text" name="subj" placeholder="Предмет">
+    <input type="text" name="theme" placeholder="Тема">
+    <textarea name="content" cols="30" rows="10" placeholder="Конспект"></textarea>
     <input type="submit">
 </form>
 
-
 </html>
-
 
 <? 
 
@@ -20,23 +18,22 @@ $link = mysqli_connect("localhost", "mysql", "mysql", "hackathon-cityheroes-123"
 
 // Получение данных из формы 
 
-$university = 'Some un.';
-$faculty = 'Faculty';
-$course = '1';
-$subj = 'Maths';
-$theme = 'Subject';
-$content = 'Content to test this function';
-
-// Проверки
+$university = $_POST['un'];
+$faculty = $_POST['faculty'];
+$course = $_POST['course'];
+$subj = $_POST['subj'];
+$theme = $_POST['theme'];
+$content = $_POST['content'];
 
 
 // Формирование новой записи
 
-$add = mysqli_query ($link, "INSERT INTO `conspect` (`university`, `faculty`, `course`, `subj`, `theme`, `content`) VALUES ('$university', '$faculty', '$course', '$subj', '$theme', '$content')");
+if (!empty($university) && !empty($faculty) && !empty($course) && !empty($subj) && !empty($theme) && !empty($content)) {
 
-// Переадресация на главную 
+    $add = mysqli_query ($link, "INSERT INTO `conspect` (`university`, `faculty`, `course`, `subj`, `theme`, `content`) VALUES ('$university', '$faculty', '$course', '$subj', '$theme', '$content')");
+    echo "<script>document.location.href='/';</script>";
 
-echo "<script>document.location.href='/http://hackathon-cityheroes-123';</script>";
+}
 
 mysqli_close($link);
 
--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <? 
 
-print ("<a href='/add.php'> Add post </a> <br>");
+print ("<a href='/add.php'> Add post </a> <br><br>");
 
 print ("all posts:");
 
 $link = mysqli_connect("localhost", "mysql", "mysql", "hackathon-cityheroes-123");
+
+$get_all = mysqli_query ($link, "SELECT * FROM `conspect` WHERE 1");
+
+while ($item = mysqli_fetch_array ($get_all)){
+    print ("Учебное заведение:".$item['university'].", ");
+    print ("Специальность:".$item['faculty'].", ");
+    print ("Курс:".$item['course'].". <br>");
+    print ("Предмет:".$item['subj'].", ");
+    print ("Тема:".$item['theme'].". <br>");
+    print ("Конспект:".$item['content']." <br><br>");
+}
+$subj = $_POST['subj'];
+$theme = $_POST['theme'];
+$content = $_POST['content'];
+
 
 mysqli_close($link);
 
