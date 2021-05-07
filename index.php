5fbcbc77dd68f0894d18898ad7b938503dc869c5
vlasova106 <62952426+vlasova106@users.noreply.github.com>
2021-05-07 14:01:38 +0600

    commit

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <? 
 
-university = '';
-faculty = '';
-year = 1;
-subj = '';
-theme = '';
-date = '';
-
-
+$university = 'Some un.';
+$faculty = 'Faculty';
+$year = 1;
+$subj = 'Maths';
+$theme = 'Subject';
+$date = '15.09.2021';
+$content = 'Content to test this function';
 
 
 
